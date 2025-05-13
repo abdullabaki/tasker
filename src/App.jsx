389af6ccdbecae763cb3ca293f5aedb81616dfeb
MD. Abdulla Hale Baki <bakimd77@gmail.com>
2025-05-13 23:39:25 +0600

    first commit-2

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+
+function App() {
+   return (
+      <div className="items-center flex flex-col">
+         <Header />
+         <Hero />
+         <Footer />
+      </div>
+   );
+}
+
+export default App;
